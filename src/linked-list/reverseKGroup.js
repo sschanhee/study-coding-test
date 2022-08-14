898ac05365a820818881ca7d1b11a4ff1d6eb3b9
daehooonkim <dhoon.kim7@gmail.com>
2022-08-14 13:44:00 +0900

    LinkedList 2nd

---
+++ src/linked-list/reverseKGroup.js
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ var reverseKGroup = function(head, k) {
+    
+    let LL = new ListNode()
+    let currentNode = LL
+    
+    let count = 0
+    let list = [];
+    
+    while (head) {
+        
+        list.push(head.val)
+        head = head.next
+        count += 1
+        
+        if (head==null && count!=k) {      
+            console.log("hi2")
+            list.forEach(function(item,index){
+                currentNode.val = item
+                if (index != list.length-1) {
+                    currentNode.next = new ListNode()
+                    currentNode = currentNode.next
+                }
+            });
+        }
+        
+        if (count == k) {
+            console.log("hi")
+            reversedList = list.sort((a,b)=>b-a)  
+            reversedList.forEach(function(item,index){
+                currentNode.val = item
+                //currentNode.next = new ListNode()
+                //currentNode = currentNode.next
+                
+                if (index < (list.length-1)) {
+                    currentNode.next = new ListNode()
+                    currentNode = currentNode.next
+                }
+            });
+            
+            if (head) {
+                currentNode.next = new ListNode()
+                currentNode = currentNode.next
+            }
+            
+            list = [];
+            count = 0;
+        }
+        
+
+        
+    }
+    
+    return LL
+    
+};
